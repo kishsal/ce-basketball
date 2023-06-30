68b8aae19445644e8f58a12c89ed79064cceba86
Kish Sal <kishan.salgado@gmail.com>
2023-06-30 13:15:15 -0400

    Added comments

--- app.py
+++ app.py
@@ -1,18 +1,22 @@
 from flask import Flask, render_template, jsonify, request
 import json
 
+# Create the application instance
 app = Flask(__name__)
 
+# Create a URL route in our application for "/"
 @app.route('/')
 def index():
     return render_template('index.html')
 
+# Create a URL route in our application for "/roster"
 @app.route('/roster')
 def roster():
     with open('data/team_roster.json', 'r') as f:
         team_roster = json.load(f)
     return render_template('roster.html', roster=team_roster)
 
+# Create a URL route in our application for "/stats"
 @app.route('/stats')
 def stats():
     with open('data/game_stats.json', 'r') as f:
@@ -20,12 +24,14 @@ def stats():
     game_dates = list(set([game['date'] for game in game_stats]))
     return render_template('stats.html', game_dates=game_dates)
 
+# Create a URL route in our application for "/schedule"
 @app.route('/schedule')
 def schedule():
     with open('data/schedule_events.json', "r") as json_file:
         schedule_events = json.load(json_file)
     return render_template('schedule.html', schedule_events=schedule_events)
 
+# Create a URL route in our application for "/api/roster"
 @app.route('/api/game-stats')
 def api_game_stats():
     game_date = request.args.get('date')
